c662ceeb5156318b56001bade11ab3e87147e67d
Bhawani Giri <bgiri0706@conestogac.on.ca>
2016-08-11 11:17:05 -0400

    Add files via upload
    The alert part is still need to be completed.

---
+++ webtest.php
@@ -0,0 +1,25 @@
+<?php
+    $people = array(
+    array('id'=>1, 'first_name'=>'John', 'last_name'=>'Smith', 'email'=>'john.smith@hotmail.com'),
+    array('id'=>2, 'first_name'=>'Paul', 'last_name'=>'Allen', 'email'=>'paul.allen@microsoft.com'),
+    array('id'=>3, 'first_name'=>'James', 'last_name'=>'Johnston', 'email'=>'james.johnston@gmail.com'),
+    array('id'=>4, 'first_name'=>'Steve', 'last_name'=>'Buscemi', 'email'=>'steve.buscemi@yahoo.com'),
+    array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
+);
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
+
+    for($i=0; $i< sizeof($people); $i++){
+    $nameEmail = "Name ".$people[$i]['first_name']." ".$people[$i]['last_name']." Email: ".$people[$i]['email'];
+    echo "<tr>";
+    echo "<td>".$people[$i]['id']."</td>";
+    echo "<td>".$people[$i]['first_name']."</td>";
+    echo "<td>".$people[$i]['last_name']."</td>";
+    echo "<td>".$people[$i]['email']."</td>";
+    echo "<td><input type='button' id='btn' onclick= 'alertName()' value='Click Here' /></tr>";
+    
+    //Sorry, count not alert name and email when button is clicked. 
+}
+ echo "</tr>";
+ echo "</table>";
